833150cf7390f82dab9d57c7ba547bcb30ed0f1c
Darshan Nandagavi <nandagavidarshan562@gmail.com>
2026-04-01 19:28:06 +0530

    backend production api added

--- server/index.js
+++ server/index.js
@@ -64,7 +64,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: "https://netflix42.vercel.app" || "http://localhost:5173",
     credentials: true,
   })
 );
