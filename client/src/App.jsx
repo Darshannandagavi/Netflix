6841f8946df241303a186f525df0ff2b09bda262
Darshan Nandagavi <nandagavidarshan562@gmail.com>
2026-04-01 21:10:23 +0530

    app.jsx changed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,58 +1,66 @@
-
 import { Routes, Route } from "react-router-dom";
+
 import GuestLayout from "./components/guestlayout/GuestLayout";
 import UserLayout from "./components/userlayout/UserLayout";
 import AdminLayout from "./components/adminlayout/AdminLayout";
+
 import Home from "./components/guestlayout/Home";
 import About from "./components/guestlayout/About";
 import Services from "./components/guestlayout/Services";
 import Contact from "./components/guestlayout/Contact";
 import Login from "./components/guestlayout/Login";
 import Register from "./components/guestlayout/Register";
+import ForgotPassword from "./components/guestlayout/ForgotPassword";
 import NotFound from "./components/guestlayout/NotFound";
+
 import UserDashboard from "./components/userlayout/UserDashboard";
 import FeedbackForm from "./components/userlayout/FeedbackForm";
 import Profile from "./components/userlayout/Profile";
+import MovieRecommender from "./components/userlayout/MoviesRecommender";
+
 import AdminDashboard from "./components/adminlayout/AdminDashboard";
 import UserFeedbacks from "./components/adminlayout/UserFeedbacks";
 import AdminUsers from "./components/adminlayout/AdminUsers";
-import ForgotPassword from "./components/guestlayout/ForgotPassword";
 import AdminContacts from "./components/adminlayout/AdminContacts";
-import MovieRecommender from "./components/userlayout/MoviesRecommender";
+
 function App() {
-  return (
-      <Routes>
+return ( <Routes>
 
-        <Route path="/" element={<GuestLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          
-          <Route path="/register" element={<Register />} />
-        </Route>
 
-        <Route path="/user" element={<UserLayout />}>
-          <Route path="dashboard" element={<UserDashboard />} />
-          <Route path="movies" element={<MovieRecommender />} />
-          <Route path="feedback" element={<FeedbackForm />} />
-          <Route path="profile" element={<Profile />} />
-        </Route>
+  {/* Guest Routes */}
+  <Route path="/" element={<GuestLayout />}>
+    <Route index element={<Home />} />
+    <Route path="about" element={<About />} />
+    <Route path="services" element={<Services />} />
+    <Route path="contact" element={<Contact />} />
+    <Route path="login" element={<Login />} />
+    <Route path="forgot-password" element={<ForgotPassword />} />
+    <Route path="register" element={<Register />} />
+  </Route>
 
-        <Route path="/admin" element={<AdminLayout />}>
-          <Route index element={<AdminDashboard />} />
-          <Route path="movies" element={<MovieRecommender />} />
-          <Route path="feedbacks" element={<UserFeedbacks />} />
-          <Route path="users" element={<AdminUsers />} />
-          <Route path="contacts" element={<AdminContacts />} />
-        </Route>
+  {/* User Routes */}
+  <Route path="/user" element={<UserLayout />}>
+    <Route path="dashboard" element={<UserDashboard />} />
+    <Route path="movies" element={<MovieRecommender />} />
+    <Route path="feedback" element={<FeedbackForm />} />
+    <Route path="profile" element={<Profile />} />
+  </Route>
 
-        <Route path="*" element={<NotFound />} />
+  {/* Admin Routes */}
+  <Route path="/admin" element={<AdminLayout />}>
+    <Route index element={<AdminDashboard />} />
+    <Route path="movies" element={<MovieRecommender />} />
+    <Route path="feedbacks" element={<UserFeedbacks />} />
+    <Route path="users" element={<AdminUsers />} />
+    <Route path="contacts" element={<AdminContacts />} />
+  </Route>
 
-      </Routes>
-  );
+  {/* Fallback */}
+  <Route path="*" element={<NotFound />} />
+
+</Routes>
+
+);
 }
 
 export default App;
